2ed0f68c56254ab629c6588afe3d167e4d22f7f8
kojoankomah <patrickankomah4@gmail.com>
2025-04-01 22:11:25 +0000

    test

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -85,7 +85,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
     const timestampInput = document.getElementById("timestamp");
     if (timestampInput) {
